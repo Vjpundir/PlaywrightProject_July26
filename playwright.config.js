3dc29d8ba99340a9d1070846e7b2d838bca3e323
Vijay Kumar Pundir <vpundir@bayonesolutions.com>
2026-07-14 21:44:38 +0530

    Minor changes done

--- playwright.config.js
+++ playwright.config.js
@@ -1,81 +1,37 @@
 // @ts-check
-import { defineConfig} from '@playwright/test';
+import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  /* Run tests in files in parallel */
+
   fullyParallel: false,
   retries: 0,
   timeout: 50 * 1000,
 
   expect: {
-    timeout: 60 * 1000
+    timeout: 60 * 1000,
   },
 
+  reporter: [
+    ['html', { open: 'never' }],
+    ['list']
+  ],
 
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  
-
-reporter: [
-  ['html', { open: 'never' }],
-  ['list']
-],
-
-  
-use: {
+  use: {
+    browserName: 'chromium',
+    headless: true,
     trace: 'retain-on-failure',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
-    headless: true
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-
-      use:
-      {
+      use: {
         browserName: 'chromium',
-        headless: !!process.env.CI
-      }
-
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
- } 
-]
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+        headless: true,
+      },
+    },
+  ],
 });
-
